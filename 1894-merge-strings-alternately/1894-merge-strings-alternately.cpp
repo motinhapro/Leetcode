3201776d94210e2033e7859856e32794790a2420
Carlos Eduardo Mota <carloseduardomotapro@gmail.com>
2025-08-18 16:32:59 -0300

    Time: 4 ms (30.5%), Space: 8.6 MB (32.19%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+
+        string ans;
+
+        int n;
+
+        word1.size() > word2.size() ? n = word1.size() : n = word2.size();
+
+        for(int i = 0; i < n; i++) {
+
+            if(i < word1.size()) ans+=word1[i];
+
+            if(i < word2.size()) ans+=word2[i];
+        }
+        
+        return ans;
+    }
+};
