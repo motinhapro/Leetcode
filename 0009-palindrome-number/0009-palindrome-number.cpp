d708eea590315a459ade4cf91290236219dc92bd
Carlos Eduardo Mota <carloseduardomotapro@gmail.com>
2025-08-18 15:04:15 -0300

    Time: 0 ms (100%), Space: 10.8 MB (20.67%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+
+        string s = to_string(x);
+
+        int i, j;
+        
+        for(i = 0, j = s.size() - 1; i < j; i++, j--) {
+
+            if(s[i] != s[j]) return false;
+        }
+
+        return true;
+    }
+};
