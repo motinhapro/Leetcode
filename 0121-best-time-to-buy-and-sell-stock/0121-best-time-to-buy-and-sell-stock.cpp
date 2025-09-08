df9fff0b179099a7c5c394805b684112002a41b6
Carlos Eduardo Mota <carloseduardomotapro@gmail.com>
2025-09-08 15:40:22 -0300

    Time: 3 ms (27.67%), Space: 97.4 MB (25.58%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        if(!prices.size()) return 0;
+        
+        int menor = prices[0], profit = 0, ans = 0;
+
+        for(int i = 1; i < prices.size(); i++) {
+
+            ans = max(ans, prices[i] - menor);
+
+            if(prices[i] < menor) {
+                menor = prices[i];
+            }
+        }
+
+        return ans;
+    }
+};
