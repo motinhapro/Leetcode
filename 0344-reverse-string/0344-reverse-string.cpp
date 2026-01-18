e5729b17a743e6f4dcbb8cfa0d106dae7a839fd5
Carlos Eduardo Mota <carloseduardomotapro@gmail.com>
2026-01-17 23:01:54 -0300

    Time: 0 ms (100%), Space: 27.1 MB (97.42%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        
-        int l = 0, r = s.size() - 1;
-        char aux = ' ';
-
-        while(l < r) {
-            aux = s[l];
-            s[l] = s[r];
-            s[r] = aux;
-            l++, r--;
-        }
+        reverse(s.begin(), s.end());
     }
 };
